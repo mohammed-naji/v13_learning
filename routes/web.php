4554c281d3ea529bf49c418c3c013574f3775f03
Mohammed Naji <m.naji1512@gmail.com>
2023-04-13 16:36:29 +0300

    authentication

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\Admin\AdminController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
 
 // 1 => 7
 // Route::group(['prefix' => 'admin', 'as' => 'name.'], function() {
@@ -9,6 +10,12 @@ use Illuminate\Support\Facades\Route;
 // });
 
 // 8 => 10
-Route::prefix('admin')->name('admin.')->group(function() {
+Route::prefix('admin')->name('admin.')->middleware('auth', 'khalid')->group(function() {
     Route::get('/', [AdminController::class, 'index'])->name('index');
 });
+
+Route::view('/', 'welcome');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
